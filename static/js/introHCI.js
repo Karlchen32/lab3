74b18038868189ee71e07b79b380b468f7dab8f2
Karl Chen <klc026@ucsd.edu>
2019-01-24 10:34:53 -0800

    lab3 done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,34 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	
+
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Klasses");
+		$("#testjs").text("Please wait. . .");
+		$(".jumbotron p").addClass("active");
 	});
+
+	$("a.thumbnail").click(projectClick);
+
+}
+
+function projectClick(e) { 
+	 e.preventDefault();
+   
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(containingProject).project-description.fadeOut()
+    }
+
+}
+
+
+
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
